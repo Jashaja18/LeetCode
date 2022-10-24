19d10cb4abe91de183b52795ccb9eae204303a28
Jashanpreet Jandu <jashanjandu23@gmail.com>
2022-10-24 15:38:38 -0700

    New Problem

--- Easy/DivisorGame.py
+++ Easy/DivisorGame.py
@@ -1,4 +1,4 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        return n % 2 == 0 # if n is even, Alice wins
+        return n % 2 == 0  # if n is even, Alice wins
     # Time Complexity: O(1)
