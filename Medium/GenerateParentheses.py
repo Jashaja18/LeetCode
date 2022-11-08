c103c7b43d4bb84aa0a299382e672f2d5633c8c7
Jashanpreet Jandu <jashanjandu23@gmail.com>
2022-11-08 14:26:44 -0800

    New Problems

--- Medium/GenerateParentheses.py
+++ Medium/GenerateParentheses.py
@@ -1,33 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        List = []
-
-        def generate(A='', left=0, right=0):
-            if len(A) == 2 * n:
-                List.append(A)
+        def backtrack(S = '', left = 0, right = 0):
+            if len(S) == 2 * n:
+                ans.append(S)
                 return
             if left < n:
-                generate(A + '(', left + 1, right)
+                backtrack(S+'(', left+1, right)
             if right < left:
-                generate(A + ')', left, right + 1)
+                backtrack(S+')', left, right+1)
 
-        generate()
-        return List
-
-        # List = []
-        # backtracking = Backtracking
-        #     if n == 2:
-        #         return n
-        #     else:
-        #         return [
-        #             y + x
-        #             for y in self.generateParenthesis(1, n)
-        #             for x in self.generateParenthesis(list - 1, n)
-        #         ]
-
-# ALGORITHM
-
-# Step 1 − if current_position is goal, return success
-# Step 2 − else,
-# Step 3 − if current_position is an end point, return failed.
-# Step 4 − else, if current_position is not end point, explore and repeat above steps.
+        ans = []
+        backtrack()
+        return ans
