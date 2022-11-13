1ebdeb13b7ea0b18aadec5fc26bb8253af523293
Jashanpreet Jandu <jashanjandu23@gmail.com>
2022-11-13 12:22:06 -0800

    New Problems

--- Easy/ReplaceElementswithGreatestElementonRightSide.py
+++ Easy/ReplaceElementswithGreatestElementonRightSide.py
@@ -1,6 +1,5 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        for i in range(len(arr)-1):
-            arr[i] = max(arr[i+1:])
-        arr[-1] = -1
-        return arr
+        hashset = set(arr)
+
+        for i in range(len(arr)):
--- Medium/CarFleet.py
+++ Medium/CarFleet.py
@@ -1,5 +1,6 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        stack = []
-
-
+        if not position:
+            return 0
+        cars = sorted(zip(position, speed))
+        times = [(target - p) / s for p, s in cars]
