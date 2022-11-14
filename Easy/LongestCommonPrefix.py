84eafc6414cddf540a7194fafe39c772f490182a
Jashanpreet Jandu <jashanjandu23@gmail.com>
2022-11-14 11:08:44 -0800

    New Problems

---
+++ Easy/LongestCommonPrefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        for i in range(len(first)):
+            if first[i] != last[i]:
+                return first[:i]
+        return first
